d138b0080c2d5cac28bfc4743303ad3faff2e0c5
Szymon Gabrek <szymon.gabrek@gmail.com>
2021-05-29 14:49:55 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/success")
+def success():
+    return render_template("success.html")
+
+@app.route("/add", methods=["POST"])
+def add():
+    # Zrób coś aby wygenerować skrócony URL i zapamiętaj adres
+    return redirect("/success")
